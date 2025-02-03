f2b4a942ddb64721db7b46e495d4208d2fca0d0e
SrimanTetali <tetalisriman@gmail.com>
2025-02-03 11:30:20 +0530

    Update in App.js to display Home page

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 
-// import React from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Navbar from "./components/navbar/Navbar";
-// import Home from "./components/Home";
-// import Doctors from "./components/Doctors";
-// import About from "./components/About";
-// import Contact from "./components/Contact";
-// import AuthForm from "./components/AuthForm";
-// // import Footer from "./components/Footer";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./components/Home";
+import Doctors from "./components/Doctors";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import AuthForm from "./components/AuthForm";
+// import Footer from "./components/Footer";
 
-// function App() {
-//   return (
-//     <Router>
-//       <Navbar />
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/doctors" element={<Doctors />} />
-//         <Route path="/about" element={<About />} />
-//         <Route path="/contact" element={<Contact />} />
-//         <Route path="/auth" element={<AuthForm />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/auth" element={<AuthForm />} />
+      </Routes>
+    </Router>
+  );
+}
 
-// export default App;
+export default App;
 
 
 
@@ -65,29 +65,30 @@
 
 
 
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Doctors from './Dashboards/Doctor/Doctor';
-import Dashboard from './Dashboards/Doctor/Dashboard';
-import Appointment from './Dashboards/Doctor/Appointment';
-import Profile from './Dashboards/Doctor/Profile';
+// import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+// import Doctors from './Dashboards/Doctor/Doctor';
+// import Dashboard from './Dashboards/Doctor/Dashboard';
+// import Appointment from './Dashboards/Doctor/Appointment';
+// import Profile from './Dashboards/Doctor/Profile';
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        {/* Redirect "/" to "/dashboard" */}
-        <Route path="/" element={<Navigate to="/dashboard" replace />} />
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         {/* Redirect "/" to "/dashboard" */}
+//         <Route path="/" element={<Navigate to="/dashboard" replace />} />
 
-        {/* Doctors layout as parent */}
-        <Route path="/" element={<Doctors />}>
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="appointments" element={<Appointment />} />
-          <Route path="profile" element={<Profile />} />
-        </Route>
-      </Routes>
-    </Router>
-  );
-}
+//         {/* Doctors layout as parent */}
+//         <Route path="/" element={<Doctors />}>
+//           <Route path="dashboard" element={<Dashboard />} />
+          
+//           <Route path="appointments" element={<Appointment />} />
+//           <Route path="profile" element={<Profile />} />
+//         </Route>
+//       </Routes>
+//     </Router>
+//   );
+// }
 
-export default App;
+// export default App;
 
